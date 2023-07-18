2f9f7cc537d361b70c358e079f54bba30254cd3b
Scott Windrem <scottwindrem@gmail.com>
2023-07-18 11:46:23 -0400

    it worksgit status need to make sure r1 is lower than r2, and finish styling

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let range2 = document.getElementById("range2");
 let check1 = document.getElementById("check1");
 let check2 = document.getElementById("check2");
 
+let fizzTxt = document.getElementById("fizzLabel");
+let buzzTxt = document.getElementById("buzzLabel");
+let fbTxt = document.getElementById("fizzBuzzLabel");
+
 let userFizz = document.getElementById("fizz");
 let userBuzz = document.getElementById("buzz");
 let userFB = document.getElementById("fizzbuzz");
@@ -11,8 +15,9 @@ let userFB = document.getElementById("fizzbuzz");
 let button = document.getElementById("button");
 
 let fin = document.getElementById("finalStr");
+let realFinalStr = document.getElementById("strOutput");
 
-//function to get an array of integers between two given integers
+//function to get an array of integers between the two given integers
 getRange = (r1, r2) => {
   let range = [];
   for (i = r1; i < r2 + 1; i++) {
@@ -21,15 +26,26 @@ getRange = (r1, r2) => {
   return range;
 };
 
+// functions to update labels
+changeLabel1 = () => {
+  fizzTxt.innerHTML = `If integer is divisble by ${check1.value}, it will be replaced by:`;
+};
+changeLabel2 = () => {
+  buzzTxt.innerHTML = `If integer is divisble by ${check2.value}, it will be replaced by:`;
+};
+changeLabel3 = () => {
+  fbTxt.innerHTML = `If integer is divisble by ${check1.value} AND ${check2.value}, it will be replaced by:`;
+};
+
 //function to update H3 with appropriate values
 changeH3 = () => {
   fin.innerHTML =
-    fin.innerHTML = `Your range is from ${range1.value} to ${range2.value}.  Check 1 is: ${check1.value},  Check 2 is: ${check2.value} `;
+    fin.innerHTML = `You will receive an output of integers from ${range1.value} to ${range2.value}.  If an integer is dvisible by ${check1.value}, it will be replaced by ${userFizz.value}.  If an integer is divisible by ${check2.value}, it will be replaced by ${userBuzz.value}.  If an integer is divisble by BOTH ${check1.value} and ${check2.value}, it will be replaced by ${userFB.value}`;
 };
 //initialize the H3 element
 changeH3();
 
-//event listeners to check that value of range1, range2, check1, or check2 has been changed and update the h3 element dynamically
+//event listeners to check that value of range1, range2, check1, or check2 has been changed and update the text elements dynamically
 range1.addEventListener("input", () => {
   changeH3();
 });
@@ -38,24 +54,34 @@ range2.addEventListener("input", () => {
 });
 check1.addEventListener("input", () => {
   changeH3();
+  changeLabel1();
+  changeLabel3();
 });
 check2.addEventListener("input", () => {
   changeH3();
+  changeLabel2();
+  changeLabel3();
 });
 
 //function to take user inputs and create a fizzbuzz out of it
 function fizzbuzz(arr) {
+  let strOut = ``;
   for (i = 0; i < arr.length; i++) {
-    if (arr[i] % 5 == 0 && arr[i] % 3 == 0) {
+    if (arr[i] % check1.value == 0 && arr[i] % check2.value == 0) {
       console.log(`${userFB.value}`);
-    } else if (arr[i] % 5 == 0) {
+      strOut += `${userFB.value}, `;
+    } else if (arr[i] % check2.value == 0) {
+      strOut += `${userBuzz.value}, `;
       console.log(`${userBuzz.value}`);
-    } else if (arr[i] % 3 == 0) {
+    } else if (arr[i] % check1.value == 0) {
+      strOut += `${userFizz.value}, `;
       console.log(`${userFizz.value}`);
     } else {
+      strOut += `${arr[i]}, `;
       console.log(arr[i]);
     }
   }
+  realFinalStr.innerHTML = strOut;
 }
 
 function tieItAllTogether() {
